1460287879d579dc2eb5650c331a3a430c4c19a0
EdgarSouSousa <edgarsousa@ua.pt>
2023-05-24 15:36:30 +0100

    Appointment Feature Added

--- Appointment.Designer.cs
+++ Appointment.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtAnimalId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVetId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAppointmentReason = new System.Windows.Forms.TextBox();
+            this.dtpAppointmentDate = new System.Windows.Forms.DateTimePicker();
+            this.lstDiagnostics = new System.Windows.Forms.ListBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lstTreatments = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtAnimalId
+            // 
+            this.txtAnimalId.Location = new System.Drawing.Point(112, 32);
+            this.txtAnimalId.Name = "txtAnimalId";
+            this.txtAnimalId.Size = new System.Drawing.Size(206, 23);
+            this.txtAnimalId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AnimalID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Vet ID";
+            // 
+            // txtVetId
+            // 
+            this.txtVetId.Location = new System.Drawing.Point(112, 61);
+            this.txtVetId.Name = "txtVetId";
+            this.txtVetId.Size = new System.Drawing.Size(206, 23);
+            this.txtVetId.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Reason";
+            // 
+            // txtAppointmentReason
+            // 
+            this.txtAppointmentReason.Location = new System.Drawing.Point(112, 90);
+            this.txtAppointmentReason.Name = "txtAppointmentReason";
+            this.txtAppointmentReason.Size = new System.Drawing.Size(206, 23);
+            this.txtAppointmentReason.TabIndex = 4;
+            // 
+            // dtpAppointmentDate
+            // 
+            this.dtpAppointmentDate.Location = new System.Drawing.Point(112, 137);
+            this.dtpAppointmentDate.Name = "dtpAppointmentDate";
+            this.dtpAppointmentDate.Size = new System.Drawing.Size(200, 23);
+            this.dtpAppointmentDate.TabIndex = 6;
+            // 
+            // lstDiagnostics
+            // 
+            this.lstDiagnostics.FormattingEnabled = true;
+            this.lstDiagnostics.ItemHeight = 15;
+            this.lstDiagnostics.Location = new System.Drawing.Point(559, 27);
+            this.lstDiagnostics.Name = "lstDiagnostics";
+            this.lstDiagnostics.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstDiagnostics.Size = new System.Drawing.Size(185, 259);
+            this.lstDiagnostics.TabIndex = 8;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(131, 274);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(113, 62);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lstTreatments
+            // 
+            this.lstTreatments.FormattingEnabled = true;
+            this.lstTreatments.ItemHeight = 15;
+            this.lstTreatments.Location = new System.Drawing.Point(363, 27);
+            this.lstTreatments.Name = "lstTreatments";
+            this.lstTreatments.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstTreatments.Size = new System.Drawing.Size(167, 259);
+            this.lstTreatments.TabIndex = 10;
+            // 
+            // Appointment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstTreatments);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lstDiagnostics);
+            this.Controls.Add(this.dtpAppointmentDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAppointmentReason);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtVetId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAnimalId);
+            this.Name = "Appointment";
             this.Text = "Appointment";
+            this.Load += new System.EventHandler(this.Appointment_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtAnimalId;
+        private Label label1;
+        private Label label2;
+        private TextBox txtVetId;
+        private Label label3;
+        private TextBox txtAppointmentReason;
+        private DateTimePicker dtpAppointmentDate;
+        private ListBox lstDiagnostics;
+        private Button btnSubmit;
+        private ListBox lstTreatments;
     }
 }
--- Appointment.cs
+++ Appointment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,169 @@ namespace VetCheck
 {
     public partial class Appointment : Form
     {
+        //iniciate SQL connection
+        private SqlConnection CN = new SqlConnection();
+
+        private SqlConnection getSGBDConnection()
+        {
+            return new SqlConnection("Data Source = " + "tcp:mednat.ieeta.pt\\SQLSERVER,8101" + " ;" + "Initial Catalog = " + "p2g1" + "; uid = " + "p2g1" + ";" + "password = " + "2TorresDeCT2001");
+        }
+
+        private bool verifySGBDConnection()
+        {
+            if (CN == null)
+                CN = getSGBDConnection();
+
+            if (CN.State != ConnectionState.Open)
+                CN.Open();
+
+            return CN.State == ConnectionState.Open;
+        }
         public Appointment()
         {
             InitializeComponent();
+        }
+
+        private void Appointment_Load(object sender, EventArgs e)
+        { // Populate treatments
+            CN = getSGBDConnection();
+            string sql = "SELECT treatment_id, name FROM Treatment";
+            if (verifySGBDConnection())
+            {
+                using (SqlCommand command = new SqlCommand(sql, CN))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int treatmentId = reader.GetInt32(0);
+                        string treatmentName = reader.GetString(1);
+                        lstTreatments.Items.Add(new TreatmentItem(treatmentId, treatmentName));
+                    }
+                    reader.Close();
+                }
+            }
+
+            // Populate diagnoses
+            sql = "SELECT diagnostic_id, name FROM Diagnostic";
+            if (verifySGBDConnection())
+            {
+                using (SqlCommand command = new SqlCommand(sql, CN))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int diagnosticId = reader.GetInt32(0);
+                        string diagnosticName = reader.GetString(1);
+                        lstDiagnostics.Items.Add(new DiagnosticItem(diagnosticId, diagnosticName));
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+        // Helper classes for ComboBox and ListBox items
+        class TreatmentItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public TreatmentItem(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        class DiagnosticItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DiagnosticItem(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            CN = getSGBDConnection();
+            if (!verifySGBDConnection())
+            {
+                MessageBox.Show("FAILED TO OPEN CONNECTION TO DATABASE", "Connection Test", MessageBoxButtons.OK);
+                return;
+            }
+
+            int appointmentId;
+            using (SqlCommand cmd = new SqlCommand("SELECT ISNULL(MAX(Appointment_id), 0) + 1 FROM Appointment", CN))
+            {
+                object result = cmd.ExecuteScalar();
+                appointmentId = (result != null) ? Convert.ToInt32(result) : 1;
+            }
+
+            int animalId = int.Parse(txtAnimalId.Text);
+            int vetId = int.Parse(txtVetId.Text);
+            DateTime appointmentDate = dtpAppointmentDate.Value;
+            string appointmentReason = txtAppointmentReason.Text;
+
+
+            // Insert appointment into the database
+            string sql = "INSERT INTO Appointment (Appointment_id ,animal_id, vet_id, Appointment_date, Appointment_reason) VALUES (@appointmentId,@animalId, @vetId, @appointmentDate, @appointmentReason); SELECT SCOPE_IDENTITY();";
+
+            if (verifySGBDConnection())
+            {
+                using (SqlCommand command = new SqlCommand(sql, CN))
+                {
+                    command.Parameters.AddWithValue("@appointmentId", appointmentId); // Change parameter name to @appointmentId
+                    command.Parameters.AddWithValue("@animalId", animalId);
+                    command.Parameters.AddWithValue("@vetId", vetId);
+                    command.Parameters.AddWithValue("@appointmentDate", appointmentDate);
+                    command.Parameters.AddWithValue("@appointmentReason", appointmentReason);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            // Insert selected treatments into Appointment_Treatment table
+            foreach (TreatmentItem item in lstTreatments.SelectedItems)
+            {
+                int treatmentId = item.Id;
+                sql = "INSERT INTO Appointment_Treatment (Appointment_id, treatment_id) VALUES (@appointmentId, @treatmentId)";
+                if (verifySGBDConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(sql, CN))
+                    {
+                        command.Parameters.AddWithValue("@appointmentId", appointmentId);
+                        command.Parameters.AddWithValue("@treatmentId", treatmentId);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            // Insert selected diagnoses into Appointment_Diagnostic table
+            foreach (DiagnosticItem item in lstDiagnostics.SelectedItems)
+            {
+                int diagnosticId = item.Id;
+                sql = "INSERT INTO Appointment_Diagnostic (Appointment_id, diagnostic_id) VALUES (@appointmentId, @diagnosticId)";
+                if (verifySGBDConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(sql, CN))
+                    {
+                        command.Parameters.AddWithValue("@appointmentId", appointmentId);
+                        command.Parameters.AddWithValue("@diagnosticId", diagnosticId);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            this.Close();
+
         }
     }
 }
